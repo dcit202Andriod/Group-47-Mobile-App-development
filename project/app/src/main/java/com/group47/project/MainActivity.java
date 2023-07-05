6d5f6ba34c2627b05e66e0646f2074f4a76bde58
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2023-07-05 08:23:26 +0000

    Update MainActivity.java

--- project/app/src/main/java/com/group47/project/MainActivity.java
+++ project/app/src/main/java/com/group47/project/MainActivity.java
@@ -1,1 +1,128 @@
+//hello world
+
+
+package com.group47.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import androidx.core.view.GravityCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import com.group47.project.adapter.CategoryFragmentPagerAdapter;
+import com.group47.project.utils.Constants;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Find the view pager that will allow the user to swipe between fragments
+        viewPager = findViewById(R.id.viewpager);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        // Set gravity for tab bar
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        assert navigationView != null;
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Set the default fragment when starting the app
+        onNavigationItemSelected(navigationView.getMenu().getItem(0).setChecked(true));
+
+        // Set category fragment pager adapter
+        CategoryFragmentPagerAdapter pagerAdapter =
+                new CategoryFragmentPagerAdapter(this, getSupportFragmentManager());
+        // Set the pager adapter onto the view pager
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        // Switch Fragments in a ViewPager on clicking items in Navigation Drawer
+        if (id == R.id.nav_home) {
+            viewPager.setCurrentItem(Constants.HOME);
+        } else if (id == R.id.nav_world) {
+            viewPager.setCurrentItem(Constants.WORLD);
+        } else if (id == R.id.nav_science) {
+            viewPager.setCurrentItem(Constants.SCIENCE);
+        } else if (id == R.id.nav_sport) {
+            viewPager.setCurrentItem(Constants.SPORT);
+        } else if (id == R.id.nav_environment) {
+            viewPager.setCurrentItem(Constants.ENVIRONMENT);
+        } else if (id == R.id.nav_society) {
+            viewPager.setCurrentItem(Constants.SOCIETY);
+        } else if (id == R.id.nav_fashion) {
+            viewPager.setCurrentItem(Constants.FASHION);
+        } else if (id == R.id.nav_business) {
+            viewPager.setCurrentItem(Constants.BUSINESS);
+        } else if (id == R.id.nav_culture) {
+            viewPager.setCurrentItem(Constants.CULTURE);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    // Initialize the contents of the Activity's options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    // This method is called whenever an item in the options menu is selected.
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
 
